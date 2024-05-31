48b8874b97665f6704040eed873da212123f19a5
Olha Papkevych <olgap366@gmail.com>
2024-05-31 12:58:45 +0200

    add bootstrap modal for todos

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 const LOCAL_STORAGE_KEY = 'todos';
 const $todoList = $('.js--todos-wrapper');
 const $form = $('.js--form');
+const $exampleModal = $('#exampleModal');
 
 $(document).ready(createTodoList);
 
@@ -13,14 +14,20 @@ function createTodoList() {
     }
 
     $form.on('submit', addTodo);
+    $exampleModal.on('show.bs.modal', openModal);
 }
 
 function createTodoElement(data) {
     let $listItem = $('<li class="todo-item"></li>').attr('id', `${data.id}`).appendTo($todoList);
 
     $('<input type="checkbox">').on({click: setChecking}).appendTo($listItem);
-    $('<span class="todo-item__description"></span>').text(data.text).appendTo($listItem);
-    $('<button class="todo-item__delete">Видалити</button>').on({click: deleteTodo}).appendTo($listItem);
+    $('<span class="todo-item__description"></span>')
+        .text(data.text)
+        .attr('data-bs-toggle', 'modal')
+        .attr('data-bs-target', '#exampleModal')
+        .attr('data-bs-whatever', `${data.text}`)
+        .appendTo($listItem);
+    $('<button class="todo-item__delete">Delete</button>').on({click: deleteTodo}).appendTo($listItem);
 
     if (data.isDone === true) {
         $listItem.addClass('todo-item--checked');
@@ -68,4 +75,15 @@ function deleteTodo(event) {
 
     $todoList.empty();
     createTodoList();
+}
+
+
+function openModal(event) {
+    let $todoItem = $(event.relatedTarget);
+
+    let todoText = $todoItem.attr('data-bs-whatever');
+    let isDone = $todoItem.parent().hasClass('todo-item--checked');
+
+    $('.modal-title').text(`${todoText}`);
+    $('.modal-body-text').text(isDone ? "Is Done" : "Need To Do");
 }
