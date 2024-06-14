1cb94689479a12a33e6eef216e3050fb77169e78
Olha Papkevych <olgap366@gmail.com>
2024-06-14 10:31:29 +0200

    change items amount at list

--- hw_24/react-app/src/components/List.jsx
+++ hw_24/react-app/src/components/List.jsx
@@ -3,10 +3,6 @@ import Item from "./Item.jsx";
 function List() {
     return (<ul className="list-group">
             <Item/>
-            <Item/>
-            <Item/>
-            <Item/>
-            <Item/>
         </ul>)
 }
 
