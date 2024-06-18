dc8523fe85483f16c309ec63e5234715a8d4ed74
Olha Papkevych <olgap366@gmail.com>
2024-06-18 17:54:51 +0200

    init hw_25 task_1-class component

---
+++ hw_25/task_1-class_component/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <main>
+            <section className="section">
+                <div className="container">
+                </div>
+            </section>
+        </main>
+    </>
+  )
+}
+
+export default App
